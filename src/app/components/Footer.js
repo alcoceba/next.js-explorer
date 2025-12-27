e2f30e72c5e2639391f5f2be6e41782ef6bfc99d
Manel <manelalcoceba@gmail.com>
2025-12-27 18:02:49 +0100

    Update version and dependency versions

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
 
   return (
     <div className={styles.footer} onClick={handleOnIconClick}>
-      made with <span>{icon}</span> for all developers - v1.7.0 / 1.1.0
+      made with <span>{icon}</span> for all developers - v1.7.1 / 1.1.1
     </div>
   );
 }
