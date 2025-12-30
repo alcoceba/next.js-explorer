a7d900b994eaf1d8f55083684bb25239d659d466
Manel <manelalcoceba@gmail.com>
2025-12-30 17:27:54 +0100

    core: unify CSS root files

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 import "./index.css";
-import "./theme.css";
 
 const div = document.createElement("div");
 document.body.appendChild(div);
